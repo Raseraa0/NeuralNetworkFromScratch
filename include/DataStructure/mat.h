696b1fc6464de3d06695a9ce87aa9bd0b07e90fd
Arthur Rasera <arthur.rasera@grenoble.fr>
2024-12-23 11:44:56 +0100

    ajout de création de matrice random

--- include/DataStructure/mat.h
+++ include/DataStructure/mat.h
@@ -2,6 +2,7 @@
 #define MAT_H
 
 #include <ostream>
+#include <random>
 #include <vector>
 
 using std::vector;
@@ -111,5 +112,32 @@ inline mat operator-(const mat& m, const mat& n) {
   }
   return res;
 }
+
+/* Namepace for matrix utils */
+namespace mat_ut {
+
+/* Return a matrix with all elem follow normal distribution */
+inline mat random_n(size_t n_rows, size_t n_cols, double mu, double sig) {
+
+  std::default_random_engine generator;
+  std::normal_distribution<double> distribution(mu, sig);
+
+  vector<vector<double>> res;
+  for (size_t i = 0; i < n_rows; i++) {
+    vector<double> line;
+    for (size_t j = 0; j < n_cols; j++) {
+      line.push_back(distribution(generator));
+    }
+    res.push_back(line);
+  }
+  return mat(res);
+}
+
+/* Return a matrix with all elem follow normal distribution 0,1 */
+inline mat random_n(size_t n_rows, size_t n_cols) {
+  return random_n(n_rows, n_cols, 0, 1);
+}
+
+} // namespace mat_ut
 
 #endif // !MAT_H
